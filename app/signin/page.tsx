be9b4828b38ac0da1bba96c0630dd1eb43318497
Santhosh Beerelli <san2beerelli@gmail.com>
2024-04-03 22:58:55 -0400

    fixed login issue

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,34 +5,36 @@ import { Spinner } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useApplicaiton } from "@/context/ApplicationProvider";
 export default function SigninPage() {
   const searchParams = useSearchParams();
   const [loading, setLoading] = useState(false);
   const { push } = useRouter();
+  const { setUserProfile } = useApplicaiton();
   useEffect(() => {
     const makeRequest = async () => {
       try {
         setLoading(true);
-        const session = searchParams.get("session") || "";
+        const session = searchParams.get("sessionId") || "";
         const response = await makeGet("validUser", "", session);
         setLoading(false);
         if (response.name !== "No User Found") {
           const obj = response;
           obj.session = session;
           window.localStorage.setItem("profile", JSON.stringify(obj));
+          setUserProfile({ type: "SETUSER", userProfile: obj });
           push("/profile");
         }
-        console.log(response);
       } catch (error) {
         console.log(error);
       }
     };
     makeRequest();
-  }, [searchParams, push]);
+  }, [searchParams, push, setUserProfile]);
 
   return (
     <Suspense>
-      <>{loading ? <Spinner color="warning" /> : null}</>;
+      <>{loading ? <Spinner color="warning" /> : null}</>
     </Suspense>
   );
 }
